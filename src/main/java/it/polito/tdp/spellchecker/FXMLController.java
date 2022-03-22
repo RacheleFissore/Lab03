342abedb38516cb592c12c76856771d925a6467e
Rachi <Rachi@172.22.173.30>
2022-03-22 15:57:24 +0100

    Versione finale

--- src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,9 +1,15 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.*;
 import java.util.ResourceBundle;
 
 import it.polito.tdp.spellchecker.model.Dictionary;
+import it.polito.tdp.spellchecker.model.RichWord;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,47 +18,74 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
 public class FXMLController {
-	
 	private Dictionary model;
 
-    @FXML
+    @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
 
-    @FXML
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
     private URL location;
 
-    @FXML
-    private Button bntClear;
+    @FXML // fx:id="bntClear"
+    private Button bntClear; // Value injected by FXMLLoader
 
-    @FXML
-    private Button bntSpellCheck;
+    @FXML // fx:id="bntSpellCheck"
+    private Button bntSpellCheck; // Value injected by FXMLLoader
 
-    @FXML
-    private ComboBox<String> cmbLingue;
+    @FXML // fx:id="cmbLingue"
+    private ComboBox<String> cmbLingue; // Value injected by FXMLLoader
 
-    @FXML
-    private Label lblRisultato;
+    @FXML // fx:id="lblRisultato"
+    private Label lblRisultato; // Value injected by FXMLLoader
 
-    @FXML
-    private Label lblTime;
+    @FXML // fx:id="lblTime"
+    private Label lblTime; // Value injected by FXMLLoader
 
-    @FXML
-    private TextArea txtInserimento;
+    @FXML // fx:id="txtInserimento"
+    private TextArea txtInserimento; // Value injected by FXMLLoader
 
-    @FXML
-    private TextArea txtWrong;
+    @FXML // fx:id="txtWrong"
+    private TextArea txtWrong; // Value injected by FXMLLoader
 
     @FXML
     void handleClear(ActionEvent event) {
-
+    	txtWrong.clear();
+    	txtInserimento.clear();
     }
 
     @FXML
     void handleSpellCheck(ActionEvent event) {
-
+    	txtWrong.clear();
+    	List<RichWord> listaRichWord = new LinkedList<RichWord>();
+    	model.loadDictionary(cmbLingue.getValue());
+    	String daControllare = txtInserimento.getText().toLowerCase();
+    	daControllare.replaceAll("[.,\\/#!$%\\^&\\*;:{}=\\-_`~()\\[\\]\"]", " ");
+    	String[] arrayParole = daControllare.split(" "); // Vado ad inserire dentro l'array tutte le parole che ottengo andando a slittare la stringa
+    													 // dagli spazi
+    	List<String> parole = new LinkedList<String>();
+    	
+    	for(int i = 0; i < arrayParole.length; i++) {
+    		parole.add(arrayParole[i]);
+    	}
+    	
+    	for(int i = 0; i < parole.size(); i++) {
+    		System.out.println(parole.get(i));
+    	}
+    	
+    	listaRichWord = model.spellCheckText(parole);
+    	int cntWrong = 0;
+    	
+    	for(RichWord r : listaRichWord) {
+    		if(r.isCorretta() == false) {
+    			txtWrong.appendText(r.getParola() + "\n");
+    			cntWrong++;
+    		}
+    	}
+    	
+    	lblRisultato.setText("The text contains " + cntWrong + " errors");
     }
 
-    @FXML
+    @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert bntClear != null : "fx:id=\"bntClear\" was not injected: check your FXML file 'Scene.fxml'.";
         assert bntSpellCheck != null : "fx:id=\"bntSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
@@ -61,19 +94,15 @@ public class FXMLController {
         assert lblTime != null : "fx:id=\"lblTime\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtInserimento != null : "fx:id=\"txtInserimento\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtWrong != null : "fx:id=\"txtWrong\" was not injected: check your FXML file 'Scene.fxml'.";
-        
+
         cmbLingue.getItems().clear();
         cmbLingue.getItems().add("English");
         cmbLingue.getItems().add("Italian");
-
     }
-
-	public void setModel(Dictionary model) {
+    public void setModel(Dictionary model) {
 		this.model = model;
 		
 	}
-
-
 }
 
 
--- src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
+++ src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -1,7 +1,83 @@
 package it.polito.tdp.spellchecker.model;
 
+import java.io.*;
+import java.util.*;
+
 public class Dictionary {
+	List<String> listaItaliano = new LinkedList<String>();
+	List<String> listaInglese = new LinkedList<String>();
+	
+	String linguaScelta;
+	
 	public void loadDictionary(String language) {
+		linguaScelta = language;
+		if(language.toLowerCase().compareTo("english") == 0) {
+			try {
+				FileReader fr = new FileReader("src/main/resources/English.txt");
+				BufferedReader br = new BufferedReader(fr);
+				String word;
+				while((word = br.readLine()) != null) {
+					listaInglese.add(word);
+				}
+				
+				br.close();
+			}
+			catch(IOException e) {
+				System.out.println("Errore nella lettura da file");
+			}
+		}
 		
+		if(language.toLowerCase().compareTo("italian") == 0) {
+			try {
+				FileReader fr = new FileReader("src/main/resources/Italian.txt");
+				BufferedReader br = new BufferedReader(fr);
+				String word;
+				while((word = br.readLine()) != null) {
+					listaItaliano.add(word);
+				}
+				
+				br.close();
+			}
+			catch(IOException e) {
+				System.out.println("Errore nella lettura da file");
+			}
+		}	
+	
+	}
+	
+	public List<RichWord> spellCheckText(List<String> inputTextList) {
+		List<RichWord> listaParole = new LinkedList<RichWord>();
+		boolean corretta = true;
+		//boolean trovata = false;
+		if(linguaScelta.toLowerCase().compareTo("italian") == 0) {
+			for(String s1 : inputTextList) {
+				if(listaItaliano.contains(s1)) {
+					corretta = true;
+				}
+				else {
+					corretta = false;
+				}
+				
+				RichWord p = new RichWord(s1, corretta);	
+				listaParole.add(p);
+			}
+		}
+		
+		if(linguaScelta.toLowerCase().compareTo("english") == 0) {
+			for(String s1 : inputTextList) {
+				
+				if(listaInglese.contains(s1)) {
+					corretta = true;
+				}
+				else {
+					corretta = false;
+				}
+					
+				RichWord p = new RichWord(s1, corretta);	
+				listaParole.add(p);
+				
+			}
+		}
+		return listaParole;
 	}
 }
